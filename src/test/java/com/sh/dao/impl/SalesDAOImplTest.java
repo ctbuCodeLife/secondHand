6e1b136b3c576ba56203a06cf4d1bb137266eee3
myq <84210278@qq.com>
2017-05-15 02:02:34 +0800

    增加订单请求的测试类

---
+++ src/test/java/com/sh/dao/impl/SalesDAOImplTest.java
@@ -0,0 +1,105 @@
+package com.sh.dao.impl; 
+
+import com.sh.dao.SalesDAO;
+import com.sh.model.Sales;
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After;
+
+import java.util.List;
+
+/** 
+* SalesDAOImpl Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>���� 15, 2017</pre> 
+* @version 1.0 
+*/ 
+public class SalesDAOImplTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: insert(Sales record) 
+* 
+*/ 
+@Test
+public void testInsert() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: delete(Integer id) 
+* 
+*/ 
+@Test
+public void testDelete() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: update(Sales record) 
+* 
+*/ 
+@Test
+public void testUpdate() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: findAll() 
+* 
+*/ 
+@Test
+public void testFindAll() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: findAllReqOrder(Integer uid) 
+* 
+*/ 
+@Test
+public void testFindAllReqOrder() throws Exception { 
+//TODO: Test goes here...
+    //测试通过
+//    SalesDAO sd = new SalesDAOImpl();
+//    List<Sales> sales = sd.findAllReqOrder(10000);
+//    for(Sales s:sales){
+//        System.out.println(s);
+//    }
+} 
+
+/** 
+* 
+* Method: select(Integer id) 
+* 
+*/ 
+@Test
+public void testSelect() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: count() 
+* 
+*/ 
+@Test
+public void testCount() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
